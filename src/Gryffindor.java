8d979d018bc54850d327778df156a63b5a0dd587
Diana <dianamingazova162@gmail.com>
2023-11-16 04:17:02 +0500

    коммит

---
+++ src/Gryffindor.java
@@ -0,0 +1,57 @@
+public class Gryffindor extends hogwarts {
+    private int nobility;
+    private int honor;
+    private int bravery;
+
+    public Gryffindor(String nameOfStudent, int power, int transgration, int nobility, int honor, int bravery) {
+        super(nameOfStudent, power, transgration);
+        this.nobility = nobility;
+        this.honor = honor;
+        this.bravery = bravery;
+    }
+
+    public int getNobility() {
+        return nobility;
+    }
+
+    public int getHonor() {
+        return honor;
+    }
+
+    public int getBravery() {
+        return bravery;
+    }
+
+    public void setNobility(int nobility) {
+        this.nobility = nobility;
+    }
+
+    public void setHonor(int honor) {
+        this.honor = honor;
+    }
+
+    public void setBravery(int bravery) {
+        this.bravery = bravery;
+    }
+
+    @Override
+    public String toString() {
+        return "Gryffindor{" +
+                "nobility=" + nobility +
+                ", honor=" + honor +
+                ", bravery=" + bravery +
+                ", nameOfStudent='" + nameOfStudent + '\'' +
+                ", power=" + power +
+                ", transgration=" + transgration +
+                '}';
+    }
+
+    public void compare(Gryffindor Gryffindor) {
+        if (nobility + honor + bravery < Gryffindor.getNobility() + Gryffindor.getHonor() + Gryffindor.getBravery()) {
+            System.out.println(nameOfStudent + " лучший Гриффиндорец, чем " + Gryffindor.getNameOfStudent());
+        } else {
+            System.out.println(Gryffindor.getNameOfStudent() + " лучший Гриффиндорец, чем " + nameOfStudent);
+        }
+    }
+
+}
---
+++ src/hogwarts.java
@@ -0,0 +1,57 @@
+import javax.lang.model.SourceVersion;
+import java.sql.SQLOutput;
+
+public class hogwarts {
+    protected String nameOfStudent;
+    protected int power;
+    protected int transgration;
+
+
+    public hogwarts(String nameOfStudent, int power, int transgration) {
+        this.nameOfStudent = nameOfStudent;
+        this.power = power;
+        this.transgration = transgration;
+    }
+
+    public String getNameOfStudent() {
+        return nameOfStudent;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getTransgration() {
+        return transgration;
+    }
+
+    public void setNameOfStudent(String nameOfStudent) {
+        this.nameOfStudent = nameOfStudent;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setTransgration(int transgration) {
+        this.transgration = transgration;
+    }
+
+    @Override
+    public String toString() {
+        return "hogwarts{" +
+                "nameOfStudent='" + nameOfStudent + '\'' +
+                ", power=" + power +
+                ", transgration=" + transgration +
+                '}';
+    }
+
+    public void compare(hogwarts hogwarts) {
+        if (power + transgration < hogwarts.getPower() + hogwarts.getTransgration()) {
+            System.out.println(nameOfStudent + " обладает бОльшей мощностью магии, чем " + hogwarts.getNameOfStudent());
+        } else {
+            System.out.println(hogwarts.getNameOfStudent() + " обладает бОльшей мощностью магии, чем " + nameOfStudent);
+        }
+    }
+}
+
